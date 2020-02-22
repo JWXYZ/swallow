10969ae4e592d67244d23e74d194eb745ce6247c
Mickey <1807121439@qq.com>
2020-02-22 22:49:30 +0800

    第二版

--- configure-client/src/main/java/com/swallow/configureclient/ConfigureClientApplication.java
+++ configure-client/src/main/java/com/swallow/configureclient/ConfigureClientApplication.java
@@ -1,8 +1,13 @@
 package com.swallow.configureclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ConfigureClientApplication {
 
@@ -10,4 +15,11 @@ public class ConfigureClientApplication {
         SpringApplication.run(ConfigureClientApplication.class, args);
     }
 
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
 }
--- ribbon/src/main/java/com/swallow/ribbon/controller/RibbonToClientController.java
+++ ribbon/src/main/java/com/swallow/ribbon/controller/RibbonToClientController.java
@@ -1,15 +1,11 @@
 package com.swallow.ribbon.controller;
 
-import com.netflix.client.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
 
 @RestController
 public class RibbonToClientController {
