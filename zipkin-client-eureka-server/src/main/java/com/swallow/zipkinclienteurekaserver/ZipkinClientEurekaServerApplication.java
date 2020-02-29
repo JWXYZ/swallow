b24d8ae7cc87dd008c8f321c791383811e72babe
Mickey <1807121439@qq.com>
2020-02-29 21:57:10 +0800

    第六版：服务端的高可用

---
+++ zipkin-client-eureka-server/src/main/java/com/swallow/zipkinclienteurekaserver/ZipkinClientEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.swallow.zipkinclienteurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinClientEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinClientEurekaServerApplication.class, args);
+    }
+
+}
