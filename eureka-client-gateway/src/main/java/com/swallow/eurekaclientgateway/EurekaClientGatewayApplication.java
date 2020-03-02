bcb74f764d5b0d61a3d3c572bd343811044e09d6
Mickey <1807121439@qq.com>
2020-03-02 23:27:16 +0800

    第八版： gateway结束

---
+++ eureka-client-gateway/src/main/java/com/swallow/eurekaclientgateway/EurekaClientGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.swallow.eurekaclientgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory requestTimeGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
+    }
+}
---
+++ eureka-client-gateway/src/test/java/com/swallow/eurekaclientgateway/EurekaClientGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swallow.eurekaclientgateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
